7f6463badd939e5e040c8206e90adae0fce4458a
Lee-Roy King <leeroywking@gmail.com>
2020-06-06 09:27:01 -0700

    updated with better error messaging and 'help'

--- terry_stops.py
+++ terry_stops.py
@@ -12,43 +12,60 @@ for item in columns:
     column_titles.append(item["name"])
 # print(column_titles)
 
-terry_stops_by_officer_race = {}
-total_terry_stops = 0
-terry_stops_by_race = {"Total":0}
+def build_results(col1:int, col2:int, col3: int):
+    outcomes_by_officer_race_and_suspect_race = {}
+    for report in data["data"]:
+        officer_race = report[col1]
+        suspect_race = report[col2]
+        outcome = report[col3]
+        out = outcomes_by_officer_race_and_suspect_race
+        try:
+            out[officer_race]
+        except:
+            out[officer_race] = {}
+        try:
+            out[officer_race][suspect_race]
+        except:
+            out[officer_race][suspect_race] = {"Total":0}
+        try:
+            out[officer_race][suspect_race][outcome]
+        except:
+            out[officer_race][suspect_race][outcome] = 1
 
-outcomes_by_officer_race_and_suspect_race = {}
-for report in data["data"]:
-    officer_race = report[17]
-    suspect_race = report[18]
-    outcome = report[12]
-    out = outcomes_by_officer_race_and_suspect_race
-    try:
-        out[officer_race]
-    except:
-        out[officer_race] = {}
-    try:
-        out[officer_race][suspect_race]
-    except:
-        out[officer_race][suspect_race] = {"Total":0}
-    try:
-        out[officer_race][suspect_race][outcome]
-    except:
-        out[officer_race][suspect_race][outcome] = 1
+        out[officer_race][suspect_race][outcome] += 1
+        out[officer_race][suspect_race]["Total"] +=1
+    return outcomes_by_officer_race_and_suspect_race
 
-    out[officer_race][suspect_race][outcome] += 1
-    out[officer_race][suspect_race]["Total"] +=1
+def get_races():
+    officer_races = set()
+    suspect_races = set()
+    for entry in data["data"]:
+        officer_races.add(entry[17])
+        suspect_races.add(entry[18])
+    return list(officer_races), (suspect_races)
 
+def print_results(officer_race: str, suspect_race: str):
+    outcomes_by_officer_race_and_suspect_race = build_results(17,18,12)
+    print(f"Outcomes for {suspect_race} suspects with {officer_race} officer")
+    for outcome in outcomes_by_officer_race_and_suspect_race[officer_race][suspect_race]:
+        parent = outcomes_by_officer_race_and_suspect_race[officer_race][suspect_race]
+        print(outcome, math.floor(parent[outcome] /parent["Total"] * 10000)/100,"%")
+    print("n= ", outcomes_by_officer_race_and_suspect_race[officer_race][suspect_race]["Total"])
+    print("")
 
-print("outcomes for Black suspects  with White officer")
-for outcome in outcomes_by_officer_race_and_suspect_race["White"]["Black or African American"]:
-    parent = outcomes_by_officer_race_and_suspect_race["White"]["Black or African American"]
-    print(outcome, math.floor(parent[outcome] /parent["Total"] * 10000)/100,"%")
-print("n= ", outcomes_by_officer_race_and_suspect_race["White"]["Black or African American"]["Total"])
+# print(len(data["data"]))
+while True:
+    officer_races, suspect_races = get_races()
+    officer_race = input("Officer race:")
+    while officer_race not in officer_races:
+        print(f"please select an option from the following list")
+        print(officer_races)
+        officer_race = input("Officer race: ")
+    suspect_race = input("Suspect race:")
+    while suspect_race not in suspect_races:
+        print(f"please select an option from the following list")
+        print(suspect_races)
+        suspect_race = input("Suspect race: ")
+    print("")
+    print_results(officer_race, suspect_race)
 
-print("")
-
-print("outcomes for White suspects  with White officer")
-for outcome in outcomes_by_officer_race_and_suspect_race["White"]["White"]:
-    parent = outcomes_by_officer_race_and_suspect_race["White"]["White"]
-    print(outcome, math.floor(parent[outcome] /parent["Total"] * 10000)/100,"%")
-print("n= ", outcomes_by_officer_race_and_suspect_race["White"]["White"]["Total"])
